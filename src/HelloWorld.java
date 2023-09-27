18dfc79f38063222e22d4e7d4e7407fdbbd60e88
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-09-27 14:57:00 -0500

    After lesson on Java Syntax, Data Types, and Variables...completed exercise to demonstrate basic java principles -KS" git push origin main

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,57 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello, World!");
-        System.out.print("Hello, World!");
+        System.out.println("Hello, World!");
+
+        int myFavoriteNumber = 25;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "3.14159";
+        System.out.println(myString);
+
+        Float myNumber = 3.14f;
+        System.out.println(myNumber);
+
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+//        x is a value of 5, on the first print it remains 5, post incremented, second print is new current value 6
+        int y = 5;
+        System.out.println(++y);
+        System.out.println(y);
+//        x is 5, preincremented to first post - posts 6, second print equals current value 6
+
+//        String class;
+//        cannot use class as variable name
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+
+        int a = 4;
+        a += 5;
+        System.out.println(a);
+
+        int b = 3;
+        int c = 4;
+        c *= b;
+        System.out.println(c);
+
+        int d = 10;
+        int e = 2;
+        d /= e;
+        e -= d;
+        System.out.println(d);
+        System.out.println(e);
+
+//        If the value is larger than the type can hold, data could be lost;
+//        If the value is less than a larger than needed type, all works fine;
+//        If you increment past a types max hold, data could be lost;
+
+
     }
 }
