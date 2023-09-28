555fee62df69cbea314c26a2a0a38aa1931b4763
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-09-28 13:30:44 -0500

    JAVA OI exercises -KS

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+
+    public static void main (String[] args) {
+//        1st eprinting data to the console
+        double pi = 3.14159;
+        System.out.printf("The value of pi is approximetely %s.%n", pi);
+
+//  Calling the scanner
+        Scanner scanner = new Scanner(System.in);
+//  Enter and print an integer
+        System.out.print("Enter an Integer: ");
+        int userInput = scanner.nextInt();
+//  Enter three words and print
+        System.out.print("Enter three words: ");
+        String input1 = scanner.next().toLowerCase();
+        String input2 = scanner.next().toLowerCase();
+        String input3 = scanner.next().toLowerCase();
+        scanner.nextLine();
+
+        System.out.println(input1);
+        System.out.println(input2);
+        System.out.println(input3);
+
+//        if less than 3 words it will not pass until 3
+//        if more than 3 words, only first three print
+
+//  Enter a sentence and print
+        System.out.print("Enter a short sentence: ");
+        String userSentence = scanner.nextLine();
+
+        System.out.println(userSentence);
+
+//  Calculate perimeter and area
+        System.out.print("Enter the classroom length in inches (just a number): ");
+        float length = Float.parseFloat(scanner.nextLine());
+        System.out.print("Enter the classroom width in inches (just a number): ");
+        float width = Float.parseFloat(scanner.nextLine());
+        System.out.print("Enter the classroom height in inches (just a number): ");
+        float height = Float.parseFloat(scanner.nextLine());
+
+//  Equations for room perimeter/area/volume
+        Float area = length * width;
+        Float perimeter = (20 * length) + (2 * width);
+        Float volume = length * width * height;
+
+        System.out.printf("The classroom perimeter is %s inches, while the area is %s inches, while the volume is %s.%n", perimeter, area, volume);
+
+
+    }
+
+}
