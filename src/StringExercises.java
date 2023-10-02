5a9e104740171c6fd7c95dec2885b7c04b238977
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-02 14:14:57 -0500

    Completed Strings Problem 1 Class String Exercises -KS

---
+++ src/StringExercises.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class StringExercises {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String fisrtblock = "We don't need no education \nWe don't need no thought control";
+        System.out.println(fisrtblock);
+
+        String secondBlock = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(secondBlock);
+
+        String thirdBlock = "I can do backshlashes \\, double backshlashes \\\\,\nand the amazing triple backshlash \\\\\\!";
+        System.out.println(thirdBlock);
+    }
+}
