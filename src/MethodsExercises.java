105fdb0f1cb2227c3d24d74c1d54c2a317e86d6e
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-03 09:41:05 -0500

    Completed 1a,b,&c of methods exercises. Created arithmatic methods, which take two parameters and were tested in the main method. -KS

---
+++ src/MethodsExercises.java
@@ -0,0 +1,32 @@
+public class MethodsExercises {
+
+    public static void main(String[] args) {
+        System.out.println(adding(3, 4));
+
+        System.out.println(subtract(2, 9));
+
+        System.out.println(multiply(3, 3));
+
+        System.out.println(divide(10, 5));
+
+    }
+
+
+//    Methods for main
+//    Adding method
+    public static int adding(int num, int num2) {
+       return num + num2;
+    }
+//    Subtracting method
+    public static int subtract(int num, int num2) {
+        return num - num2;
+    }
+//    Multiplication Method
+    public static int multiply(int num, int num2) {
+        return num * num2;
+    }
+//    Division method
+    public static int divide(int num, int num2) {
+        return num / num2;
+    }
+}
