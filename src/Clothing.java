724ccd9213267bb9c2f4fb6ea06c1c46d15f2e18
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-04 09:20:45 -0500

    Completed the morning warmup for Object Oriented Programming lesson refresh/first try -KS

---
+++ src/Clothing.java
@@ -0,0 +1,26 @@
+public class Clothing {
+
+    public String type;
+    public String color;
+    public String size;
+
+    public String sayClothesType() {
+        return String.format("The %s is %s, in the size %s.%n", type, color, size);
+    }
+
+
+    public static void main(String[] args) {
+
+        Clothing shirt = new Clothing();
+        shirt.type = "shirt";
+        shirt.color = "blue";
+        shirt.size = "medium";
+        System.out.println(shirt.sayClothesType());
+
+        Clothing pant = new Clothing();
+        pant.type = "pant";
+        pant.color = "dark blue";
+        pant.size = "32";
+        System.out.println(pant.sayClothesType());
+    }
+}
