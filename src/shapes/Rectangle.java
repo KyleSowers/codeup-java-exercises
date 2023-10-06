902022d6bc40e988a5dbc869d65956c546248872
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-06 15:57:28 -0500

    Created class Rectangle in package Shapes that set two properties (lenght and width), then a constructor accepting length/width and sets the properties. -KS

---
+++ src/shapes/Rectangle.java
@@ -0,0 +1,14 @@
+package shapes;
+
+public class Rectangle {
+
+//    Properties
+    protected int length;
+    protected int width;
+
+//    Constructor
+    public Rectangle(int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
+}
